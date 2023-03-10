b8768d00b4ebad146f81f7bdb0857c5e495e6a5f
rishi1213d <113300014+rishi1213d@users.noreply.github.com>
2023-03-10 22:22:33 +0530

    Create radixsort.java

---
+++ week2task/radixsort.java
@@ -0,0 +1,40 @@
+public static void radixSort(int[] arr) {
+    int maxVal = getMaxVal(arr);
+
+    for (int exp = 1; maxVal / exp > 0; exp *= 10) {
+        countSort(arr, exp);
+    }
+}
+
+private static int getMaxVal(int[] arr) {
+    int maxVal = arr[0];
+    for (int i = 1; i < arr.length; i++) {
+        if (arr[i] > maxVal) {
+            maxVal = arr[i];
+        }
+    }
+    return maxVal;
+}
+
+private static void countSort(int[] arr, int exp) {
+    int n = arr.length;
+    int[] output = new int[n];
+    int[] count = new int[10];
+
+    for (int i = 0; i < n; i++) {
+        count[(arr[i] / exp) % 10]++;
+    }
+
+    for (int i = 1; i < 10; i++) {
+        count[i] += count[i - 1];
+    }
+
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[(arr[i] / exp) % 10] - 1] = arr[i];
+        count[(arr[i] / exp) % 10]--;
+    }
+
+    for (int i = 0; i < n; i++) {
+        arr[i] = output[i];
+    }
+}
